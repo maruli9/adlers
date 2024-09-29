9267f1098528c78a8716db8cf0ad79d7cc1d2f0d
maruli9 <118851819+maruli9@users.noreply.github.com>
2024-09-29 19:15:35 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    const demo = document.getElementById("demo").innerHTML = 'Początek sezonu można zaliczyć za wyjątkowo udany dla wszystkich fanow Adlersów. Bo oto przed tygodniem - dopiero co powstała drużyna - zaskoczyła swoją postawą i zabrała punkty przeciwnikowi, który na papierze wyżej był plasowany. Jeśli dodam, że kilku zawodników było nie w pełni dysponowanych i już nawet w pierwszym spotkaniu jeden z kluczowych zawodników, mianowicie Jarek, doznał kontuzji (tak zwana rwa kulszowa) to wynik remisowy należy uznać za duży sukces tej młodej obiecującej drużyny'
+    
+    const magia =document.getElementById("myDIV").style.display = "none";
+    //const znikus = document.getElementById('znika').style.display ='block';
+    DeviceMotionEvent.style.d
+  }
+  
